3d5c0a50eb7cbfea63d64c28fbdf6e653e2d7fbf
Valentine <kerubovalentine1@gmail.com>
2023-08-25 11:04:53 +0300

    install BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>Hi</>
+    <>
+      Hi
+    </>
   );
 }
 
