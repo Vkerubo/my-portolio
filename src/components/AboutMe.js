b41e53152b7c02aa70e2fc5dfbb26936d32731a1
Valentine <kerubovalentine1@gmail.com>
2023-09-01 14:42:48 +0300

    define ABoutMe container

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,6 +2,15 @@ import React from "react";
 import AboutMeImage from "../components/assets/Val Image.jpeg";
 import { styled } from "styled-components";
 
+const AboutMeContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 40px;
+  background-color: #fff;
+`;
+
 function AboutMe() {
     return (
         <>Hi</>
