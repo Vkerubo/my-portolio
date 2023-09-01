8ca2c28673ba517a9f0d2abe7b71a6d35aacdf3c
Valentine <kerubovalentine1@gmail.com>
2023-09-02 00:00:10 +0300

    render projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,16 +9,38 @@ const ProjectsTitle = styled.div`
   font-style: normal;
   font-weight: 700;
   line-height: normal;
-  margin-left: 50px;
+  margin-left: 100px;
+  margin-top: 50px;
+
+  @media (max-width: 768px) {
+    margin-left: 50px;
+    margin-top: 20px;
+  }
 `;
 
 const Container = styled.div`
   display: flex;
-  justify-content: space-between;
+  //justify-content: space-between;
+  margin-left: 50px;
+  margin-top: 60px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: start;
+    gap: 20px;
+  }
 `;
 
 const LeftColumn = styled.div`
-  width: 50%;
+  width: 30%;
+  margin-top: 100px;
+  margin-left: 50px;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-top: 1px;
+    margin-left: 0px;
+  }
 `;
 
 const Title = styled.div`
@@ -36,7 +58,7 @@ const Description = styled.div`
   font-size: 15px;
   font-style: normal;
   font-weight: 400;
-  line-height: normal;
+  line-height: 1.5;
   margin-top: 10px;
 `;
 
@@ -51,6 +73,7 @@ const LiveDemoButton = styled.a`
   justify-content: flex-end;
   align-items: center;
   flex-shrink: 0;
+  margin-top: 10px;
   text-decoration: none;
   color: #56a8a1;
   background-color: transparent;
@@ -69,24 +92,78 @@ const LiveDemoButton = styled.a`
 `;
 
 const ChamaImageStyled = styled.img`
-  max-width: 100%;
+  max-width: 90%;
   max-height: 100%;
+  margin-left: 10px;
+  margin-right: 10px;
+  box-shadow: 10px 10px 4px 0px rgba(0, 0, 0, 0.35);
+
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-right: 20px;
+    margin-left: 1px;
+    height: 150px;
+  }
 `;
 
 
 
 const RightColumn = styled.div`
-  width: 50%;
+  width: 60%;
   display: flex;
+  margin-right: 10px;
   justify-content: center;
   align-items: center;
-  box-shadow: 10px 10px 4px 0px rgba(0, 0, 0, 0.35);
+
+  @media (max-width: 768px) {
+    width: 100%
+  }
 `;
 
 function Projects() {
   return (
     <>
-    <ProjectsTitle>My Projects</ProjectsTitle><br/>
+      <ProjectsTitle>My Projects</ProjectsTitle>
+      <Container>
+        <LeftColumn>
+          <Title>Chama-Ke</Title>
+          <Description>
+            Group Project. This is a Chama (merry-go-round) website that allows
+            users to find chama mates and contribute money monthly for a course.
+            It fetches from a complex API we created. It’s integrated with
+            Daraja API for payments.
+          </Description>
+          <LiveDemoButton
+            href="https://glittering-hotteok-56297e.netlify.app/"
+            target="_blank"
+          >
+            Live Demo
+          </LiveDemoButton>
+        </LeftColumn>
+        <RightColumn>
+          <ChamaImageStyled src={ChamaIMage} alt="Chama" />
+        </RightColumn>
+      </Container>
+      <Container>
+        <LeftColumn>
+          <Title>Chama-Ke</Title>
+          <Description>
+            Group Project. This is a Chama (merry-go-round) website that allows
+            users to find chama mates and contribute money monthly for a course.
+            It fetches from a complex API we created. It’s integrated with
+            Daraja API for payments.
+          </Description>
+          <LiveDemoButton
+            href="https://glittering-hotteok-56297e.netlify.app/"
+            target="_blank"
+          >
+            Live Demo
+          </LiveDemoButton>
+        </LeftColumn>
+        <RightColumn>
+          <ChamaImageStyled src={ChamaIMage} alt="Chama" />
+        </RightColumn>
+      </Container>
     </>
   );
 }
