f9d2a0c734b9eabfed4881dc5dd52b60e95c11f6
Valentine <kerubovalentine1@gmail.com>
2023-09-01 21:38:31 +0300

    render container and title

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,8 +2,27 @@ import React from "react";
 import { styled } from "styled-components";
 import ChamaIMage from "../components/assets/chama.png";
 
+const ProjectsTitle = styled.div`
+  color: #333;
+  font-family: Montserrat;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  margin-left: 50px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
 function Projects() {
-  return <>Hi</>;
+  return (
+    <>
+    <ProjectsTitle>My Projects</ProjectsTitle><br/>
+    </>
+  );
 }
 
 export default Projects;
