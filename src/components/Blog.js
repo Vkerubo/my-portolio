bf8cf3294fc65c140a105c01019debe9eb7bb578
Valentine <kerubovalentine1@gmail.com>
2023-09-02 12:32:49 +0300

    create Blog

---
+++ src/components/Blog.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { styled } from "styled-components";
+import BlogImage from "../components/assets/blogImage.png";
+
+function Blog() {
+  return <>Hi</>;
+}
+
+export default Blog;
