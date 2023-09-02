62459ceffe154d7de5f8a8fb62df79f132ea3f99
Valentine <kerubovalentine1@gmail.com>
2023-09-02 12:51:37 +0300

    define follow hashnode column

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -60,6 +60,15 @@ const BlogText = styled.div`
   line-height: normal;
 `;
 
+const FollowHashnodeColumn = styled.div`
+  width: 206px;
+  height: 14px;
+  flex-shrink: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
 
 
 function Blog() {
