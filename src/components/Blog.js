755623104878a3316af25b3ac9481bef456ebb4b
Valentine <kerubovalentine1@gmail.com>
2023-09-02 12:49:16 +0300

    define blog container

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,21 @@
 import React from "react";
 import { styled } from "styled-components";
 import BlogImage from "../components/assets/blogImage.png";
+import BlogLogo from "../components/assets/Hashnode.png";
+import BlogNavbar from "../components/assets/HahnodeNavbar.png";
+
+const BlogContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background: rgba(161, 228, 211, 0.14);
+  width: 913px;
+  height: 148px;
+  flex-shrink: 0;
+  margin: 20px auto;
+  padding: 20px;
+  box-shadow: 0px 4px 4px 0px #56a8a1;
+`;
+
 
 function Blog() {
   return <>Hi</>;
