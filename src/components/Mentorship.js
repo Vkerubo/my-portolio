1c6804327ad5098bd0257583dce2be1c6b163edd
Valentine <kerubovalentine1@gmail.com>
2023-09-02 18:24:35 +0300

    create Mentorship component

---
+++ src/components/Mentorship.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { styled } from "styled-components";
+import InstagramIcon from "../components/assets/Instagram Circle.png";
+import LinkedInIcon from "../components/assets/LinkedIn Circled.png";
+import EmailIcon from "../components/assets/Circled Envelope.png";
+import XIcon from "../components/assets/TwitterX (1).png";
+import SlackIcon from "../components/assets/Slack New.png";
+
+function Mentorship() {
+    return <div>Mentorship</div>;
+};
+
+export default Mentorship;
