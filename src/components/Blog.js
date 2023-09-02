723ea31e0de258559718c207125e0e64234ce801
Valentine <kerubovalentine1@gmail.com>
2023-09-02 15:23:57 +0300

    render blog image content

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -6,20 +6,24 @@ import BlogNavbar from "../components/assets/HahnodeNavbar.png";
 
 const BlogContainer = styled.div`
   display: flex;
-  justify-content: space-between;
-  background: rgba(161, 228, 211, 0.14);
-  width: 913px;
-  height: 148px;
+  gap: 50px;
   flex-shrink: 0;
-  margin: 20px auto;
-  padding: 20px;
-  box-shadow: 0px 4px 4px 0px #56a8a1;
+  margin-left: 140px;
+  margin-top: 50px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: start;
+    gap: 10px;
+    margin-left: 30px;
+  }
 `;
 
 const ImageColumn = styled.div`
   width: 400px;
   height: 396px;
   flex-shrink: 0;
+  position: relative;
 `;
 
 const BlogImageStyled = styled.img`
@@ -33,12 +37,52 @@ const FollowButton = styled.div`
   box-shadow: 0px 4px 4px 0px #56a8a1;
   display: flex;
   width: 69px;
+  color: #fff;
+  font-size: 10px;
   height: 17px;
   padding: 3px 5px 2px 5px;
   justify-content: center;
   align-items: center;
   flex-shrink: 0;
   cursor: pointer;
+  position: absolute; /* Position the button absolutely within the image container */
+  top: 213px; /* Position from the top */
+  left: 175px; /* Position from the left */
+`;
+
+const BlogLogoStyled = styled.img`
+  position: absolute; /* Position the button absolutely within the image container */
+  top: 113px; /* Position from the top */
+  left: 147px; /* Position from the left */
+  width: 100px;
+`;
+
+const BlogNavStyled = styled.img`
+  position: absolute; /* Position the button absolutely within the image container */
+  top: 143px; /* Position from the top */
+  left: 47px; /* Position from the left */
+  width: 100px;
+`;
+
+const BackgroundContainer = styled.div`
+  background: rgba(161, 228, 211, 0.14);
+  display: flex;
+  gap: 50px;
+  height: 180px;
+  flex-shrink: 0;
+  margin-left: 14px;
+  margin-top: 90px;
+  max-width: 100%;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: start;
+    gap: 10px;
+    margin-top: 70px;
+    max-width: 100%;
+    height: 120px;
+    margin-left: 0px;
+  }
 `;
 
 const BlogTextColumn = styled.div`
@@ -49,6 +93,14 @@ const BlogTextColumn = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
+  max-width: 100%;
+  margin-left: 45px;
+  margin-top: 40px;
+
+  @media (max-width: 768px) {
+    margin-left: 0px;
+    margin-top: 1px;
+  }
 `;
 
 const BlogText = styled.div`
@@ -58,6 +110,12 @@ const BlogText = styled.div`
   font-style: normal;
   font-weight: 500;
   line-height: normal;
+  //margin-top: 300px;
+
+  @media (max-width: 768px) {
+    margin-top: 1px;
+    max-width: 100%;
+  }
 `;
 
 const FollowHashnodeColumn = styled.div`
@@ -66,8 +124,15 @@ const FollowHashnodeColumn = styled.div`
   flex-shrink: 0;
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
+  //justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+
+  @media (max-width: 768px) {
+    margin-top: 0px;
+    margin-left: 90px;
+    //justify-content: center;
+  }
 `;
 
 const FollowHashnodeText = styled.div`
@@ -78,13 +143,36 @@ const FollowHashnodeText = styled.div`
   font-weight: 500;
   line-height: normal;
   text-decoration: underline;
-  background: #56a8a1;
-  width: 165.003px;
-  height: 1px;
+  margin-top: 40px;
+
+  @media (max-width: 768px) {
+    margin-top: 0px;
+  }
 `;
 
 function Blog() {
-  return <>Hi</>;
+  return (
+    <BlogContainer>
+      <ImageColumn>
+        <BlogImageStyled src={BlogImage} alt="Blog" />
+        <FollowButton>Follow Me</FollowButton>
+        <BlogLogoStyled src={BlogLogo} alt="Blog logo" />
+        <BlogNavStyled src={BlogNavbar} alt="Blog profile"/>
+      </ImageColumn>
+      <BackgroundContainer>
+        <BlogTextColumn>
+          <BlogText>
+            As a passionate technical writer, I love creating articles that
+            educate and inform. I believe that great technical writing can
+            change lives by spreading knowledge and improving technology.
+          </BlogText>
+        </BlogTextColumn>
+        <FollowHashnodeColumn>
+          <FollowHashnodeText>Follow me on Hashnode</FollowHashnodeText>
+        </FollowHashnodeColumn>
+      </BackgroundContainer>
+    </BlogContainer>
+  );
 }
 
 export default Blog;
