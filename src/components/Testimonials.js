3b4648b2c5d0a64727ca8dda7e2a82f08dabe77e
Valentine <kerubovalentine1@gmail.com>
2023-09-03 13:12:03 +0300

    import and define Testimonial components

---
+++ src/components/Testimonials.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { styled } from "styled-components";
+import OpeningSpeechMarks from "../components/assets/Quote Left.png";
+import ClosingSpeechMarks from "../components/assets/Get Quote.png";
+import TestimonialQuote from "../components/assets/Vector (1).svg";
+
+function Testimonials() {
+  return <>Hi</>;
+}
+
+export default Testimonials;
