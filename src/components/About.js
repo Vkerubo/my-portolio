54d144dddee8ccfcecfec6c69c89378284f42298
Valentine <kerubovalentine1@gmail.com>
2023-09-07 02:41:09 +0300

    create and render AboutMeContent

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,13 @@ import React from "react";
 import Navbar from "./Navbar";
 import Contact from "./Contact";
 import Footer from "./Footer";
+import AboutMeContent from "./AboutMeContent";
 
 function About() {
   return (
     <>
       <Navbar />
+      <AboutMeContent />
       <Contact />
       <Footer />
     </>
