a64084eb2f5eb7aae09ca5124d027a566783de1a
Valentine <kerubovalentine1@gmail.com>
2023-09-07 02:32:34 +0300

    define and render About component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./components/Homepage";
+import About from "./components/About";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
---
+++ src/components/About.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { styled } from "styled-components";
+
+function About() {
+  return <>Hi</>;
+}
+
+export default About;
