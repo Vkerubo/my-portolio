dc5a28ae078b5dec5fbba68d82c4557b607caf39
Valentine <kerubovalentine1@gmail.com>
2023-09-07 02:37:51 +0300

    render contact and footer

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Contact from "./Contact";
+import Footer from "./Footer";
 
 function About() {
   return (
-  <>
-    <Navbar />
-  </>
+    <>
+      <Navbar />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
