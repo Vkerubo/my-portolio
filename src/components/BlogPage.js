4d3039b096fc7232dae06c937d88e2d2f7c092a9
Valentine <kerubovalentine1@gmail.com>
2023-09-07 12:30:25 +0300

    render blog

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,15 +1,28 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import BlogContent from "./BlogContent";
+import Blog from "./Blog";
+import { styled } from "styled-components";
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh; /* Minimum height of the viewport */
+`;
+
+const Content = styled.div`
+  flex-grow: 1; /* Allow content to grow and fill available space */
+`;
 
 function BlogPage() {
   return (
-    <>
+    <MainContainer>
       <Navbar />
-      <BlogContent />
+      <Content>
+        <Blog />
+      </Content>
       <Footer />
-    </>
+    </MainContainer>
   );
 }
 
