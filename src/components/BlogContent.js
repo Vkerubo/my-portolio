998fcc80ed933a8ba3e327cf612aa029e04257b3
Valentine <kerubovalentine1@gmail.com>
2023-09-07 12:14:31 +0300

    define BlogTitle

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,6 +1,16 @@
 import React from "react";
 import { styled } from "styled-components";
 
+
+const BlogTitle = styled.h1`
+  color: #333;
+  font-family: Montserrat;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
 function BlogContent() {
   return <>Hi</>;
 }
