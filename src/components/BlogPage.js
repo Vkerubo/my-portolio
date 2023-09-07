13ca3e3aa3c39990820484a1fe3d6018fb60fccb
Valentine <kerubovalentine1@gmail.com>
2023-09-07 12:12:07 +0300

    Render blogcontent

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import BlogContent from "./BlogContent";
 
 function BlogPage() {
   return (
     <>
       <Navbar />
+      <BlogContent />
       <Footer />
     </>
   );
